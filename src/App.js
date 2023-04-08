9842e0af37a8edb0ac990fcd64b62ccf5090c5e9
AywinK <115905373+AywinK@users.noreply.github.com>
2023-04-08 08:19:07 +0100

    very basic working version. need to explore other react hooks for improvements

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import CreatePost from "./components/pages/CreatePost";
 import Home from "./components/pages/Home";
+import { useEffect, useState } from "react";
+import { collectionRef } from "./components/helperFuncs/firebaseConfig";
+import { onSnapshot, query, orderBy } from "firebase/firestore";
+import BlogPage from "./components/BlogPage";
 
 function App() {
+
+  const [blogsArr, setBlogsArr] = useState([]);
+
+  useEffect(() => {
+    const queryBlogs = query(collectionRef, orderBy("createdAt", "desc"));
+    const unsubscribeSnap = onSnapshot(queryBlogs, (snap) => {
+        const blogs = [];
+        snap.forEach((doc) => {
+            blogs.push({ ...doc.data(), id: doc.id });
+        });
+        setBlogsArr(blogs);
+        console.table(blogsArr);
+    });
+    return () => unsubscribeSnap();
+}, []);
+
+console.log(blogsArr);
+
   return (
     <>
       <BrowserRouter>
@@ -14,6 +36,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/createpost" element={<CreatePost />}></Route>
+          {blogsArr.map(blog => {
+            const {title, id, textContentArr, imageURL} = blog;
+            return (
+              <Route key={id} path={`/${id}`} element={<BlogPage title={title} imageURL={imageURL} textContentArr={textContentArr} id={id} />}></Route>
+            )
+          })}
         </Routes>
       </BrowserRouter>
     </>
--- src/components/FeedCard.jsx
+++ src/components/FeedCard.jsx
@@ -1,54 +1,62 @@
 
 import { Card, CardBody, CardFooter, Heading, Image, Stack, Text, Button } from '@chakra-ui/react';
+import getIDHandleClick from "./helperFuncs/getIDHandleClick";
+import { Link } from 'react-router-dom';
 
 function FeedCard(props) {
 
-    const { title, imageURL, textContentArr } = props;
+  const { id, title, imageURL, textContentArr } = props;
 
-    const handleHover = () => "";
 
-    return (
-<Card
-  className='card-grow'
-  direction="row-reverse"
-  overflow='hidden'
-  variant='outline'
-  my={2}
-  h={360}
-  minW={320}
->
-  <Image
-    objectFit='cover'
-    w="45%"
-    src={imageURL}
-    alt={title}
-  />
 
-  <Stack
-  my={1}
-  >
-    <CardBody>
-      <Heading
-      size='md'
-      >{title}</Heading>
 
-      <Text
-      py='2'
-      maxH="15ch"
-      noOfLines={5}
-      textOverflow="ellipsis"
-      overflow="hidden"
-      >{textContentArr[0]}</Text>
-    </CardBody>
+  return (
+    <Link to={id}>
+      <Card
+        className='card-grow'
+        direction="row-reverse"
+        overflow='hidden'
+        variant='outline'
+        my={2}
+        h={360}
+        minW={320}
+        data-id={id}
+        onClick={getIDHandleClick}
+      >
 
-    {/* <CardFooter>
+        <Image
+          objectFit='cover'
+          w="45%"
+          src={imageURL}
+          alt={title}
+        />
+
+        <Stack
+          my={1}
+        >
+          <CardBody>
+            <Heading
+              size='md'
+            >{title}</Heading>
+
+            <Text
+              py='2'
+              maxH="15ch"
+              noOfLines={5}
+              textOverflow="ellipsis"
+              overflow="hidden"
+            >{textContentArr[0]}</Text>
+          </CardBody>
+
+          {/* <CardFooter>
       <Button variant='solid' colorScheme='blue'>
         Buy Latte
       </Button>
     </CardFooter> */}
-  </Stack>
-</Card>
-    )
+        </Stack>
+      </Card>
+    </Link>
+  )
 }
 
 export default FeedCard;
