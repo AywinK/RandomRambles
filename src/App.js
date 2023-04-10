10c63917aa1b159b3fa6c6d5d066528a55f1ad56
AywinK <115905373+AywinK@users.noreply.github.com>
2023-04-10 05:36:03 +0100

    improves ui for suspense fallback. uses spread operator passing props into BlogPage

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { collectionRef } from "./components/helperFuncs/firebaseConfig";
 import { onSnapshot, query, orderBy } from "firebase/firestore";
 // import BlogPage from "./components/BlogPage";
 import { lazy, Suspense } from 'react';
+import { Spinner } from "@chakra-ui/spinner";
+import { Center } from "@chakra-ui/layout";
 
 const BlogPage = lazy(() => import("./components/BlogPage"));
 
@@ -46,14 +48,14 @@ function App() {
           pages={[{ name: "Home", route: "/" }, { name: "Create Post", route: "/createpost" }]}
         />
 
-        <Suspense fallback={<h1>Loading... Please Wait</h1>}>
+        <Suspense fallback={<Center h="100vh" w="100vw"><Spinner size="xl" label="Loading... Please Wait" top="50%" left="50%" ></Spinner></Center>}>
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/createpost" element={<CreatePost />}></Route>
             {blogsArr.map(blog => {
-              const { title, id, textContentArr, imageURL } = blog;
+              const { title, id } = blog;
               return (
-                <Route key={id} path={`/${title}`} element={<BlogPage setRecentlyViewedBlogPage={setRecentlyViewedBlogPage} title={title} imageURL={imageURL} textContentArr={textContentArr} id={id} />}></Route>
+                <Route key={id} path={`/${title}`} element={<BlogPage setRecentlyViewedBlogPage={setRecentlyViewedBlogPage} {...blog} />}></Route>
               )
             })}
           </Routes>
